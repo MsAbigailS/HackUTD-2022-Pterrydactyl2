dd5fb96ea29db8cd2d6235ed3bd49feb1067c8c3
ElysaTorres <elysantorres2003@gmail.com>
2022-11-12 18:52:39 -0600

    my second commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
@@ -8,15 +8,36 @@ function App() {
         <div class="navbar">
           <p>This is navbar. Here is where phone, map, bluetooth will go.</p>
         </div>
-
-        <div class="sub-navbar">
-          <p>This is where apps, mapping, notices buttons will go</p>
+             
+        <div id="subnavbar">
+            <div class="button" width="60" height="100"> Apps </div>
+            <div class="button" width="60" height="100"> Interior </div>
+            <div class="button" width="60" height="100"> Alerts </div>
+            <div class="button" width="60" height="100"> Settings </div>
+            <div class="button" width="60" height="100"> Info </div>
         </div>
       </header>
 
+        
       <body>
-        <p>This is where the currently selected display will go.</p>
+        <div id="row1">
+          
+          <div>
+            <c> Car Info </c>
+            <p> Car Model: Toyota Supra </p>
+            <p> Car Year: 2022 </p>
+            <p> Car Color: Renaissance Red 2.0 </p>
+            <p> Miles per Gallon: 25 MPG </p>
+            <p> Mileage: 20,000 miles </p>
+            <p> Performance: 255 </p>
+            <p> Horsepower: @ 5,000 - 6,5000 rpm </p>
+          </div>
+          <img src="./red toyota supra 2022.png" alt=""/>
+        </div>
 
+      
+
+        
       </body>
     </div>
   );

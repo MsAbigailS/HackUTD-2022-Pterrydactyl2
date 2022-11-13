eadfb6e203719b233668f11a471b029eb3da7b86
MsAbigailS <msabigailrsmith@gmail.com>
2022-11-12 19:26:33 -0600

    seat toggle colors active

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+/* eslint-disable jsx-a11y/alt-text */
 import logo from './logo.svg';
 import './App.css';
+import CarSeat from './CarSeat';
+import { useState } from 'react';
 
 function App() {
+
+      /* Creating states for seat clicks */
+      const [isActive, setIsActive] = useState(false);
+
+      /* When clicked, toggle state */
+      const handleClick = e => {
+      setIsActive(current => !current)
+      console.log(e.currentTarget.id)
+      console.log(isActive)
+      if(e.currentTarget.id === "driver" && isActive){
+        if(e.currentTarget.style.background === "red"){
+          e.currentTarget.style.background = "blue"
+        } else {
+          e.currentTarget.style.background = "red"
+        }
+      } else {
+        e.currentTarget.style.background = "red"
+      }
+      
+    };
+
+
   return (
     <div className="App">
       <header className="App-header">
+
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,1,200" />
         
-        
         <div class="navbar">
-          <button type="button" class="col-4" id="gps-button">
+          <button type="button" class="col-2" id="gps-button">
             <i class="material-symbols-rounded">explore</i>
           </button>
-          <button type="button" id="bluetooth-button">
+          <button type="button" class="col-2" id="bluetooth-button" >
             <i class="material-symbols-rounded">bluetooth</i>
           </button>
-          <div class="media-player-navigation">
+        </div>
+        <div class="col-5 media-player-navigation">
             <button type="button" id="previous-media-button">
               <i class="material-symbols-rounded">arrow_back_ios</i>
             </button>
@@ -24,8 +51,7 @@ function App() {
             <button type="button" id="next_media_button">
               <i class="material-symbols-rounded">arrow_forward_ios</i>
             </button>
-          </div>
-
+          
           <button type="button" id="thermostat-button">
             <i class="material-symbols-rounded">thermostat</i>
           </button>
@@ -41,6 +67,25 @@ function App() {
 
       <body>
         <p>This is where the currently selected display will go.</p>
+
+        <div class="interior-map">
+          <div class="front-seats col-4">
+            <img id="driver" src="https://static.thenounproject.com/png/4939260-200.png" alt="car seat image"
+              
+              onClick={handleClick}
+            />
+          </div>
+          
+        </div>
+
+
+
+      <h1> TESTING CARSEAT </h1>
+
+
+      <h1>
+        jfeiowajf
+      </h1>
 
       </body>
     </div>

aa7b55765c3aded6972197a74735c33c18a77488
Damian Aguilar <damianaguilar83@gmail.com>
2022-11-12 20:22:16 -0600

    First push

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
@@ -14,8 +14,34 @@ function App() {
         </div>
       </header>
 
+      
+
       <body>
-        <p>This is where the currently selected display will go.</p>
+        <div id="row1">  
+          <span>
+          <img src="./issues_images/engtempwarning.jpg" alt=""/>
+          <p id="anything">your issue is you suck</p>
+          <a href="https://www.toyotaguru.us/yaris-manual-2/engine-coolant-temperature-indicator-and-warning-light.html"><code>(solution)</code> </a>
+          </span>
+        </div>
+
+
+        <div id="row2">  
+        <span>
+        <img src="./issues_images/batterycharge.webp" alt=""/>
+          <p id="anything">your issue is you suck</p>
+          <a href="https://www.baierltoyota.com/blog/what-does-the-red-battery-light-in-your-toyota-mean/">solution </a>
+          </span>
+        </div>
+
+        <div id="row3">  
+        <span>
+           <img src="./issues_images/oilpressure.webp" alt=""/>
+          <p id="anything">your issue is you suck</p>
+          <a href="https://support.toyota.com/s/article/How-often-should-I-ma-10042?language=en_US">solution </a>
+        </span>
+       
+        </div>
 
       </body>
     </div>
